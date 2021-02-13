ea4d056043e4cb8f84c4b5f42ca42bd8ac7760cd
tylerbcirillo <tylercirillodev@gmail.com>
2021-02-13 09:26:54 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a60e06b2-66b8-4286-8a02-70e56e306ac1';
+export const MODAL_CLOSE_SEC = 2.5;
