fb9d5a7d27fe45de3f8337ddfe83a200abf26836
tylerbcirillo <tylercirillodev@gmail.com>
2021-02-13 09:31:11 -0500

    New feature

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -9,7 +9,7 @@ export default class View {
    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Jonas Schmedtmann
+   * @author Tyler B. Cirillo
    * @todo Finish implementation
    */
   render(data, render = true) {
